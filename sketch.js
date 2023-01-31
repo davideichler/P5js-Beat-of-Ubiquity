5a59e576d135b3d29c0869d5aaf8e6efc7de5fa6
davideichler <80835619+davideichler@users.noreply.github.com>
2023-01-31 19:57:33 +0100

    Neue Visuals

--- sketch.js
+++ sketch.js
@@ -6,16 +6,25 @@ var felsen;
 var kapelle;
 var linach;
 var tanne;
-var brendVisual;
-var brendGif1;
-var brendGif2
-var brendGif3
-var felsenGif;
-var kapelleGif1;
-var kapelleGif2;
-var kapelleGif3;
-var linachGif1;
-var linachGif2;
+
+var bregMoshed;
+var brendMoshed;
+var eisMoshed;
+var felsenMoshed;
+var kapelleMoshed;
+var linachtalMoshed;
+// var tanneMoshed
+
+// var brendVisual;
+// var brendGif1;
+// var brendGif2
+// var brendGif3
+// var felsenGif;
+// var kapelleGif1;
+// var kapelleGif2;
+// var kapelleGif3;
+// var linachGif1;
+// var linachGif2;
 var tanneGif1;
 var tanneGif2;  
 
@@ -35,40 +44,48 @@ var volHistoryBreg = []; */
 var env;
 
 function preload() {
-  breg = loadSound("BREG_V2.wav");
-  brendArp = loadSound("BREND_ARP_V1.wav");
-  brendBass = loadSound("BREND_BASS_V1.wav");
+  breg = loadSound("/soundsmp3/BREG_V2.mp3");
+  //brendArp = loadSound("/soundsmp3/BREND_ARP_V1.wav");
+  brendBass = loadSound("/soundsmp3/BREND_BASS_V1.mp3");
   eis = loadSound("EIS_V1.wav");
-  felsen = loadSound("FELSEN_SNARE_V1.wav");
-  kapelle = loadSound("KAPELLE_V1.wav");
-  linach = loadSound("LINACHTALSPERRE_V1.wav");
-  tanne = loadSound("TANNE_KICK_V1.wav");
+  felsen = loadSound("/soundsmp3/FELSEN_SNARE_V1.mp3");
+  kapelle = loadSound("/soundsmp3/KAPELLE_V1.mp3");
+  linach = loadSound("/soundsmp3/LINACHTALSPERRE_V1.mp3");
+  tanne = loadSound("/soundsmp3/TANNE_KICK_V1.mp3");
 
-  brendVisual = loadImage("Brend.png");
-  brendGif1 = loadImage("/GIFS/Brend1Cut.gif");
-  brendGif2 = loadImage("/GIFS/Brend2Cut.gif");
-  brendGif3 = loadImage("/GIFS/Brend3Cut.gif");
-  felsenGif = loadImage("/GIFS/Felsen3.gif");
-  kapelleGif1 = loadImage("/GIFS/Kapelle1Cut.gif");
-  kapelleGif2 = loadImage("/GIFS/Kapelle2Cut.gif")
-  kapelleGif3 = loadImage("/GIFS/Kapelle3Cut.gif")
-  kapelleGif3 = loadImage("/GIFS/Kapelle3Cut.gif");
-  linachGif1 = loadImage("/GIFS/Linach1Cut.gif");
-  linachGif2 = loadImage("/GIFS/Linach2Cut.gif");
+  bregMoshed = loadImage("/Edwin/breg_moshed.gif");
+  brendMoshed = loadImage("/Edwin/brend_moshed.gif");
+  eisMoshed = loadImage("/Edwin/eis_moshed.gif");
+  felsenMoshed = loadImage("/Edwin/felsen_moshed.gif");
+  kapelleMoshed = loadImage("/Edwin/kapelle_moshed.gif");
+  linachtalMoshed = loadImage("/Edwin/linachtalsperre_moshed.gif");
+  // tanneMoshed = loadImage("/Edwin/tanne_moshed.gif");
   tanneGif1 = loadImage("/GIFS/Tanne1Cut.gif");
-  tanneGif2 = loadImage("/GIFS/Tanne2Cut.gif");
+  // tanneGif2 = loadImage("/GIFS/Tanne2Cut.gif");
+
+  // brendVisual = loadImage("Brend.png");
+  // brendGif1 = loadImage("/GIFS/Brend1Cut.gif");
+  // brendGif2 = loadImage("/GIFS/Brend2Cut.gif");
+  // brendGif3 = loadImage("/GIFS/Brend3Cut.gif");
+  // felsenGif = loadImage("/GIFS/Felsen3.gif");
+  // kapelleGif1 = loadImage("/GIFS/Kapelle1Cut.gif");
+  // kapelleGif2 = loadImage("/GIFS/Kapelle2Cut.gif")
+  // kapelleGif3 = loadImage("/GIFS/Kapelle3Cut.gif")
+  // kapelleGif3 = loadImage("/GIFS/Kapelle3Cut.gif");
+  // linachGif1 = loadImage("/GIFS/Linach1Cut.gif");
+  // linachGif2 = loadImage("/GIFS/Linach2Cut.gif");  
 }
 
 function keyReleased() {
   switch (keyCode) {
-    case 65:
+    case 32:
       fadeAudio(breg);
       bregPressed = false;
       break;
-    case 83:
-      fadeAudio(brendArp);
-      brendPressed = false;
-      break;
+    // case 83:
+    //   fadeAudio(brendArp);
+    //   brendPressed = false;
+    //   break;
     case 68:
       fadeAudio(brendBass);
       brendPressed = false;
@@ -98,15 +115,17 @@ function keyReleased() {
 
 function keyTyped() {
 
+  console.log(keyCode);
+
   switch (keyCode) {
-    case 65:
+    case 32:
       playAudio(breg);
       bregPressed = true;
       break;
-    case 83:
-      playAudio(brendArp);
-      brendPressed = true;
-      break;
+    // case 83:
+    //   playAudio(brendArp);
+    //   brendPressed = true;
+    //   break;
     case 68:
       playAudio(brendBass);
       brendPressed = true;
@@ -159,55 +178,34 @@ function drawImage(_image) {
 
 function draw() {
   background(0);
+  if (bregPressed == true) {
+    image(bregMoshed, 0, 0, 1280, 720);
+  }
   
   if (brendPressed == true) {
-    image(brendGif3, 490, 44, 300, 632);
+    image(brendMoshed, 0, 0, 1280, 720);
+  }
+
+  if (eisPressed == true) {
+    image(eisMoshed, 0, 0, 1280, 720)
   }
 
   if (kapellePressed == true) {
     //image(kapelleGif2, 247, 6, 786, 768);
-    image(kapelleGif1, 0, 0, 1280, 720)
+    image(kapelleMoshed, 0, 0, 1280, 720)
   }
 
   if (linachPressed == true) {
-    image(linachGif1, 65, 70, 1170, 570);
+    image(linachtalMoshed, 0, 0, 1280, 720);
   }
 
   if (felsenPressed == true) {
-    image(felsenGif, 0, 4, 1280, 716)
+    image(felsenMoshed, 0, 0, 1280, 720)
   }
 
   if (tannePressed == true) {
-    image(tanneGif1, 382.5, 5, 515, 710)
+    image(tanneGif1, 0, 0, 1280, 720)
   }
- 
-// yPos = yPos - 1; 
-  // if (yPos < 0) {
-  //   yPos = height;
-  // }
-  // image(brendVisual, 10, yPos, 248, 350);
-
-
-  
-
-  // image(kapelleGif3, 500, 10);
-  // image(kapelleGif2, 300, 400);
-  // image(linachGif1, 800, 10);
-  // image(tanneGif1, 100, 400);
-  // image(felsenGif, 600, 330);
-  
-
-  // image(kapelleVisual, 300, 10, 350, 248);
-  // image(linachVisual, 270, 250, 350, 248);
-  // image(tanneVisual, 220, 70, 248, 351);
-
-  /* var vol = ampBreg.getLevel();
-  volHistoryBreg.push(vol);
-  stroke(255);
-  for (let index = 0; index < volHistoryBreg.length; index++) {
-    var y = map(volHistoryBreg[index], 0, 1, height, 0)
-    point(index, volHistoryBreg[index]);
-  } */
 
 }
 
